8532a3afc1273e1ff89ee385e139fcc21c43e422
rithika1462 <rithika1462@gmail.com>
2023-01-22 21:57:08 +0530

    Initial commit

---
+++ src/shivapackege/Ganeshclass.java
@@ -0,0 +1,52 @@
+package shivapackege;
+
+public class Ganeshclass {
+	
+	static int ak = 567;
+	static String rt = "hi";
+	static char th = 'h';
+	static double ui = 6.98;
+	static float tk = 7.89652134f;
+
+	public static void main(String[] args) {
+		System.out.println(tk);
+		System.out.println(ui);
+		System.out.println(th);
+		System.out.println(rt);
+		System.out.println("helloganesh");
+		Ganeshclass a = new Ganeshclass();
+		a.abc();
+		a.xyz();
+		a.mno();
+		Ganeshclass b = new Ganeshclass();
+		b.abc();
+	}
+
+	public static void abc() {
+		System.out.println(ak);
+		float h = 1.23498706f;
+		System.out.println(h);
+		String s = "hello 123";
+		System.out.println(s);
+		System.out.println("helloganesh1");
+	}
+
+	public void xyz() {
+		String k = "hello 789";
+		char r = 'g';
+		System.out.println(r);
+		System.out.println(k);
+		System.out.println("helloganesh3");
+
+	}
+
+	public void mno() {
+		double t = 1.40;
+		System.out.println(t);
+		int e = 3;
+		System.out.println(e);
+		System.out.println("helloganesh4");
+
+	}
+
+}
---
+++ src/shivapackege/Test.java
@@ -0,0 +1,21 @@
+package shivapackege;
+
+import java.util.TreeSet;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeSet treeSet = new TreeSet();
+		//treeSet.add(null);
+		treeSet.add("Z");
+		treeSet.add("G");
+		treeSet.add("C");
+		treeSet.add("O");
+		treeSet.add(5);
+		treeSet.add("O");
+		System.out.println("treeset is "+treeSet);
+
+	}
+
+}
