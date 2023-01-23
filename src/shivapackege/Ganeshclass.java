7a9e18a0f93da8c36eaaf5873e14d0e35e90cedf
rithika1462 <rithika1462@gmail.com>
2023-01-23 18:09:39 +0530

    abcdef

--- src/shivapackege/Ganeshclass.java
+++ src/shivapackege/Ganeshclass.java
@@ -20,7 +20,7 @@ public class Ganeshclass {
 		a.mno();
 		Ganeshclass b = new Ganeshclass();
 		b.abc();
-		System.out.println("helloman5g");
+		System.out.println("helloman6g");
 	}
 
 	public static void abc() {
